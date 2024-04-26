5837bb5319b6a653dfdc8b40c98c3cd83bdd6384
Brian.Foxi <brian.raharjo@foxilive.com>
2024-04-26 20:37:29 +0700

    test

--- src/main/java/com/example/xsis/controller/HelloworldController.java
+++ src/main/java/com/example/xsis/controller/HelloworldController.java
@@ -15,6 +15,29 @@ public class HelloworldController {
         return new MessageRes("Hello World");
     }
 
+    @GetMapping("/api/v1/Hello3")
+    public int[] getTest2(){
+        int[] input = {10, 30, 20, 40};
+        int[] output = new int[2];
+
+        for(int j=0; j < input.length; j++ ){
+            for(int i=0; i < input.length; i++ ){
+                if( i < (input.length - 1 ) ){
+                    if( input[i] > input[i+1] ){
+                        int tmp = input[i];
+                        input[i] = input[i+1];
+                        input[i+1] = tmp;
+                    }
+                }
+            }
+        }
+
+        output[0] = input[input.length - 1];
+        output[1] = input[input.length - 2];
+
+        return output;
+    }
+
     @GetMapping("/api/v1/Hello2")
     public MessageRes getTest(){
         double pinjaman = 30000000;
